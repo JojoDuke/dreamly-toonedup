e6977a1f33bd6877b8034eb24b690df51430223d
JojoDuke <jojoamankwa@gmail.com>
2025-04-30 00:16:02 +0000

    better auth url added

--- toonify-dreamer/src/lib/auth-client.ts
+++ toonify-dreamer/src/lib/auth-client.ts
@@ -1,8 +1,12 @@
 import { createAuthClient } from "better-auth/react";
 import { magicLinkClient } from "better-auth/client/plugins";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+// TODO: Change to production URL
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3001/api/auth",
+    baseURL: `${process.env.BETTER_AUTH_URL}/api/auth`,
     plugins: [
         magicLinkClient()
     ]
