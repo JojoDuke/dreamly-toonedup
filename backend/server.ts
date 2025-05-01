fdf95a6e846da7196d14254a330ad0031ef1b7ac
JojoDuke <jojoamankwa@gmail.com>
2025-05-01 14:34:23 +0000

    checking event again

--- backend/server.ts
+++ backend/server.ts
@@ -290,7 +290,7 @@ app.post('/api/edit-image', (req: Request, res: Response) => {
 app.post('/webhook/all-dodo-payments', async (req: Request, res: Response) => {
   console.log('--- DODO WEBHOOK HANDLER ENTERED ---'); 
   const event = req.body; 
-  console.log('--- DODO WEBHOOK EVENT ---',event.data.object);
+  console.log('--- DODO WEBHOOK EVENT ---', event);
   res.status(200).send('OK'); 
 
   try {
