b2be7dc3a5b4af5be171ba865aebbf4f3f9f841e
JojoDuke <jojoamankwa@gmail.com>
2025-05-21 16:26:12 +0000

    new styles

---
+++ toonify-dreamer/src/lib/stylePrompts.ts
@@ -0,0 +1,62 @@
+export const stylePrompts: Record<string, string> = {
+  // Anime
+  ghibli: "Turn this image into Ghibli anime style",
+  onePiece: "Turn this image into One Piece anime style",
+  dragonBall: "Turn this image into Dragon Ball Z anime style",
+  ninetiesAnime: "Turn this image into 90s anime style",
+  aot: "Turn this image into Attack on Titan anime style",
+
+  // Art
+  pixel: "Do this in a 16 bit pixel art style",
+  sketch: "Make this look like its a hand drawn pencil sketch art",
+  watercolor: "Make this look like its a watercolor painting",
+  manga: "Make this look like its a manga",
+  comicBook: "Make this look like its a comic book",
+  comicBookCover: "Make this look like its a comic book cover",
+
+  // Cartoons
+  simpsons: `Turn this into the Simpsons style, now remember, 
+  in the Simpsons universe, white people have yellow skin, 
+  asians have light yellow skin, blacks have black skin, 
+  indians have brown skin and mexicans/hispanics also have yellow skin. 
+  And make sure not to write any \'Simpsons\' or \'The Simpsons\' text anywhere`,
+  rickMorty: "Turn this into the Rick and Morty style",
+  familyGuy: "Turn this into the Family Guy style",
+  pixar: "Make this in the pixar 3d style, make sure to make the look and proportions cartoony and not too realistic",
+  
+  // Toys
+  lego: `Make this look like its a lego, in the 3D plasticy lego style,
+  now remember, in the Lego universe, white people have yellow skin, 
+  asians have light yellow skin, blacks have black skin, 
+  indians have brown skin and mexicans/hispanics also have yellow skin.`,
+  
+  // Other
+  thinker: `Make a photorealistic image of the statue The Thinker in its iconic pose but instead
+   make the face this face that you see here, make sure to put it in an angle that allows us
+  to see the face well`,
+  muppets: "Make this look like its a muppet",
+  
+  // Fashion Model
+  model: `Take this face and make a photorealistic image where they are a model posing for a clothing brand,
+    the background is clear white background with realistic shadows and lighting and it should be a full
+    body pose seeing them from head to toe, the clothes they wear should be what you see in the image provided,
+    if the image provided doesn\'t show all their clothes then the parts that arent shown you can just generate
+    what you think would best suit or be there.`,
+
+  // Pop Culture
+  moviePoster: `Take this image and put it in a 70s movie poster style, with random text at the bottom 
+  and around the poster with things like cast name, directors name, studio and other stuff like that, none of these 
+  have to be real by the way`,
+  retro80s: "Do this in a retro 80s neon retro wave synthwave style",
+
+  // Athlete
+  footballer: "Turn this image into a photorealistic image of a footballer/soccer player",
+  nbaStar: "Turn this image into a photorealistic image of a NBA star",
+  nflPlayer: "Turn this image into a photorealistic image of a NFL player",
+  tennisPlayer: "Turn this image into a photorealistic image of a tennis player",
+  baseballPlayer: "Turn this image into a photorealistic image of a baseball player",
+  cricketPlayer: "Turn this image into a photorealistic image of a cricket player",
+  trackRunner: "Turn this image into a photorealistic image of a track runner",
+  sumoWrestler: "Turn this image into a photorealistic image of a sumo wrestler",
+  wwe: "Turn this image into a photorealistic image of a WWE wrestler",
+}; 
--- toonify-dreamer/src/pages/Index.tsx
+++ toonify-dreamer/src/pages/Index.tsx
@@ -39,45 +39,13 @@ import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 import { Link } from 'react-router-dom';
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger, SheetClose } from "@/components/ui/sheet";
+import { stylePrompts } from '@/lib/stylePrompts';
 
 // Use Vite's import.meta.env for frontend environment variables
 // Use the VITE_ prefixed variable name
 const BACKEND_BASE_URL = import.meta.env.VITE_BETTER_AUTH_URL;
 const WIZARD_IMAGE_URL = "https://i.imgur.com/B7ptMnm.png";
 const GALAXY_IMAGE_URL = "/images/theGalazy.png";
-
-const stylePrompts: Record<string, string> = {
-  // Anime
-  ghibli: "Turn this image into Ghibli anime style",
-  // Art
-  pixel: "Do this in a 16 bit pixel art style",
-  sketch: "Make this look like its a hand drawn pencil sketch art",
-  watercolor: "Make this look like its a watercolor painting",
-  // Cartoons
-  simpsons: `Turn this into the Simpsons style, now remember, 
-  in the Simpsons universe, white people have yellow skin, 
-  asians have light yellow skin, blacks have black skin, 
-  indians have brown skin and mexicans/hispanics also have yellow skin. 
-  And make sure not to write any 'Simpsons' or 'The Simpsons' text anywhere`,
-  rickMorty: "Turn this into the Rick and Morty style",
-  familyGuy: "Turn this into the Family Guy style",
-  pixar: "Make this in the pixar 3d style, make sure to make the look and proportions cartoony and not too realistic",
-  // Toys
-  lego: `Make this look like its a lego, in the 3D plasticy lego style,
-  now remember, in the Lego universe, white people have yellow skin, 
-  asians have light yellow skin, blacks have black skin, 
-  indians have brown skin and mexicans/hispanics also have yellow skin.`,
-  // Other
-  thinker: `Make a photorealistic image of the statue The Thinker in its iconic pose but instead
-   make the face this face that you see here, make sure to put it in an angle that allows us
-  to see the face well`,
-  // Fashion Model
-  model: `Take this face and make a photorealistic image where they are a model posing for a clothing brand,
-    the background is clear white background with realistic shadows and lighting and it should be a full
-    body pose seeing them from head to toe, the clothes they wear should be what you see in the image provided,
-    if the image provided doesn't show all their clothes then the parts that arent shown you can just generate
-    what you think would best suit or be there.`,
-};
 
 const Index = () => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
